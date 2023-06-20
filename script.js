926a22658d38459bcb60465f34ee24e1c396e8de
Hakeem <12151899q@gmail.com>
2023-06-20 11:30:40 -0700

    many edits to index.html

---
+++ script.js
@@ -0,0 +1,54 @@
+// Questions
+var questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Rome", "Madrid"],
+      correctAnswer: 0
+    },
+    {
+      question: "Which programming language is used for web development?",
+      choices: ["JavaScript", "Python", "Java", "C++"],
+      correctAnswer: 0
+    },
+    {
+        question: "What is an Array?",
+        choices: ["Variable", "Data structure", "HTML", "Java"],
+        correctAnswer: 1
+    },
+    {
+        question: "What state does NOT border Mexico?",
+        choices: ["California", "Texas", "Utah", "Arizona"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the capital of New York?",
+        choices: ["New York", "Los Angeles", "Buffalo", "Albany"],
+        correctAnswer: 3
+      },
+    {
+        question: "Who wrote the book "A song of Ice and Fire?",
+        choices: ["Stephen King", "George RRM", "JK Rowling", "CS Lewis"],
+        correctAnswer: 1
+      },
+    {
+        question: "What year was Donald Trump born?",
+        choices: ["1946", "1940", "1850", "1955"],
+        correctAnswer: 0
+      },
+    {
+        question: "Who won the 2010 NBA Finals?",
+        choices: ["Nuggets", "Spurs", "Clippers", "Lakers"],
+        correctAnswer: 3
+      },
+    {
+        question: "What element was named after a Roman god?",
+        choices: ["Hydrogen", "Titanium", "Mercury", "Carbon"],
+        correctAnswer: 0
+      },
+    {
+        question: "What caused the Black Plague ?",
+        choices: ["Bacteria", "War", "Illuminati", "Donald Trump"],
+        correctAnswer: 0
+      },
+
+  ];
