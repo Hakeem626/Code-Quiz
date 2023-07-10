e4dc95549f00aa71755ec2f9c0165f65e990da22
Hakeem <12151899q@gmail.com>
2023-07-10 11:29:05 -0700

    added scores.js and scores.html

---
+++ scores.js
@@ -0,0 +1,31 @@
+// DOM element
+var scoresListElement = document.getElementById("scores-list");
+
+// Fetch high scores and populate the table
+function fetchHighScores() {
+  // Here, you can fetch the high scores using your preferred method (e.g., API call, local storage, etc.)
+  // Replace this example data with your actual high scores
+  var highScores = [
+    { initials: "HO", score: 100 },
+    { initials: "CD", score: 80 },
+    { initials: "EF", score: 60 }
+  ];
+
+  // Clear the scores list
+  scoresListElement.innerHTML = "";
+
+  // Add each high score to the table
+  highScores.forEach(function(score) {
+    var row = document.createElement("tr");
+    var initialsCell = document.createElement("td");
+    var scoreCell = document.createElement("td");
+    initialsCell.textContent = score.initials;
+    scoreCell.textContent = score.score;
+    row.appendChild(initialsCell);
+    row.appendChild(scoreCell);
+    scoresListElement.appendChild(row);
+  });
+}
+
+// Fetch and display high scores on page load
+fetchHighScores();
--- script.js
+++ script.js
@@ -1,3 +1,100 @@
+// DOM elements
+var startButton = document.getElementById("start-btn");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+var feedbackElement = document.getElementById("feedback");
+var timeLeftElement = document.getElementById("time-left");
+
+// Quiz variables
+var currentQuestionIndex = 0;
+var timeLeft = 60;
+var timerInterval;
+var score = 0;
+
+// Event listener for start button
+startButton.addEventListener("click", startQuiz);
+
+// Function to start the quiz
+function startQuiz() {
+  startButton.disabled = true;
+  startTimer();
+  showQuestion();
+}
+
+// Function to start the timer
+function startTimer() {
+  timerInterval = setInterval(function() {
+    timeLeft--;
+    timeLeftElement.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      endQuiz();
+    }
+  }, 1000);
+}
+
+// Function to display the current question
+function showQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+
+  // Clear previous choices
+  choicesElement.innerHTML = "";
+
+  // Create and append answer choice buttons
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var choice = currentQuestion.choices[i];
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.setAttribute("data-index", i);
+    choiceButton.addEventListener("click", handleAnswer);
+    choicesElement.appendChild(choiceButton);
+  }
+}
+// Function to handle user's answer
+function handleAnswer(event) {
+  var selectedChoice = event.target;
+  var selectedAnswerIndex = parseInt(selectedChoice.getAttribute("data-index"));
+  var currentQuestion = questions[currentQuestionIndex];
+
+  if (selectedAnswerIndex === currentQuestion.correctAnswer) {
+    feedbackElement.textContent = "Correct!";
+    score++;
+  } else {
+    feedbackElement.textContent = "Wrong!";
+    timeLeft -= 10; // Subtract 10 seconds for an incorrect answer
+  }
+
+  // Move to the next question
+  currentQuestionIndex++;
+
+  // Check if all questions are answered
+  if (currentQuestionIndex === questions.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+}
+
+// Function to end the quiz
+function endQuiz() {
+  clearInterval(timerInterval);
+  questionElement.textContent = "Quiz Over!";
+  choicesElement.innerHTML = "";
+  feedbackElement.textContent = "";
+
+  // Prompt for initials and save score
+  var initials = prompt("Enter your initials:");
+  saveScore(initials, score);
+}
+
+// Function to save the score
+function saveScore(initials, score) {
+  // Save the initials and score using your preferred method (e.g., local storage, API call, etc.)
+  console.log("Initials: " + initials);
+  console.log("Score: " + score);
+}
+
 // Questions
 var questions = [
     {
@@ -26,7 +123,7 @@ var questions = [
         correctAnswer: 3
       },
     {
-        question: "Who wrote the book "A song of Ice and Fire?",
+        question: `Who wrote the book "A song of Ice and Fire?"`,
         choices: ["Stephen King", "George RRM", "JK Rowling", "CS Lewis"],
         correctAnswer: 1
       },
@@ -50,5 +147,4 @@ var questions = [
         choices: ["Bacteria", "War", "Illuminati", "Donald Trump"],
         correctAnswer: 0
       },
-
-  ];
+];
